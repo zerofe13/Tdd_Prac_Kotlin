ad070fa603c6733e02e73194e84cd97cf8fd871c
zerofe13 <zerofe13@gmail.com>
2023-08-30 10:32:59 +0900

    Entity

--- src/main/kotlin/com/example/memberko/domain/Membership.kt
+++ src/main/kotlin/com/example/memberko/domain/Membership.kt
@@ -1,7 +1,36 @@
 package com.example.memberko.domain
 
-import jakarta.persistence.Entity
+import jakarta.persistence.*
+import org.hibernate.annotations.ColumnDefault
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.time.LocalDateTime
 
 @Entity
-class Membership (){
+class Membership (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null,
+
+    @Enumerated(EnumType.STRING)
+    var membershipType: MembershipType,
+
+    @Column(nullable = false)
+    var userId: String,
+
+    @Column(nullable = false)
+    var point: Int = 0,
+
+    @CreationTimestamp
+    @Column(nullable = false, length = 20, updatable = false)
+    var creatAt: LocalDateTime? = null,
+
+    @UpdateTimestamp
+    @Column(length = 20)
+    var updateAt: LocalDateTime? = null
+
+) {
+    override fun toString(): String {
+        return "Membership(id=$id, membershipType=$membershipType, userId='$userId', point=$point, creatAt=$creatAt, updateAt=$updateAt)"
+    }
 }
---
+++ src/main/kotlin/com/example/memberko/domain/MembershipType.kt
@@ -0,0 +1,7 @@
+package com.example.memberko.domain
+
+enum class MembershipType(val companyName: String) {
+    NAVER("네이버"),
+    LINE("라인"),
+    KAKAO("카카오")
+}
