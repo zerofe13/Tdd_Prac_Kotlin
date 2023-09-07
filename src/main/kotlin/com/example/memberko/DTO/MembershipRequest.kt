e947373c1227fd30eb8c88b315d8eb292cd60fa5
zerofe13 <zerofe13@gmail.com>
2023-09-07 15:24:40 +0900

    valid

--- src/main/kotlin/com/example/memberko/DTO/MembershipRequest.kt
+++ src/main/kotlin/com/example/memberko/DTO/MembershipRequest.kt
@@ -1,9 +1,13 @@
 package com.example.memberko.DTO
 
 import com.example.memberko.domain.MembershipType
+import jakarta.validation.constraints.Min
 import org.jetbrains.annotations.NotNull
 
 class MembershipRequest (
+    @field:NotNull
+    @field:Min(0)
     val point: Int,
+    @field:NotNull
     val membershipType: MembershipType
 )
